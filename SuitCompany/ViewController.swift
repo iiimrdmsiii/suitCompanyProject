a6e6084d84df83be15ac35a83bc34612dbbfbb56
dallin smuin <dallinsmuin@gmail.com>
2019-10-13 16:36:57 -0600

    Initial Commit

---
+++ SuitCompany/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuitCompany
+//
+//  Created by Dallin Smuin on 10/13/19.
+//  Copyright © 2019 Dallin Smuin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
